e8e7b77a6357ed10b766f767a1dfdfd9d394f521
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2023-04-07 00:36:07 -0400

    Add image list
    - Add scrollable image list below the prompt
    - Add refresh image button
    - 'Gimme a new suggestion' button now works

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,24 +1,109 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { useRecoilValue } from "recoil";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ListRenderItem,
+} from "react-native";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import imagesState from "../state/imageState";
 import suggestionState from "../state/suggestionState";
+import { ImageUrl } from "../types/imageUrl";
+import { fetchImages, fetchSuggestion } from "../fetchData";
+// import { fetchImage } from "../utils/fetchImage"; // Import fetchImage from the correct module
 
 const Body = () => {
   const images = useRecoilValue(imagesState);
   const suggestion = useRecoilValue(suggestionState);
 
+  const setImages = useSetRecoilState(imagesState);
+  const setSuggestion = useSetRecoilState(suggestionState);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Submit:", inputValue);
+    setInputValue("");
+  };
+
+  const handleUseSuggestion = () => {
+    setInputValue(suggestion);
+  };
+
+  const handleRefreshSuggestion = async () => {
+    const newSuggestion = await fetchSuggestion();
+    setSuggestion(newSuggestion ?? "");
+  };
+
+  const handleRefreshImage = async () => {
+    const newImages = await fetchImages();
+    setImages(newImages ?? []);
+  };
+
+  const renderItem: ListRenderItem<ImageUrl> = ({ item }) => {
+    return (
+      <View key={item.name}>
+        <Image
+          source={{ uri: item.url }}
+          style={{ width: "100%", height: undefined, aspectRatio: 1 }}
+          resizeMode="cover"
+        />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Suggestion: {suggestion}</Text>
-      {images.map((image) => {
-        console.log(image.name);
-        return (
-          <View key={image.name}>
-            <Text>{image.name.split("_")[0].trim()}</Text>
-          </View>
-        );
-      })}
+    <View className="flex-1">
+      <View className="flex flex-col items-center justify-center mx-4 mb-2">
+        <TextInput
+          className="w-full h-15 bg-white border border-gray-300 rounded p-2"
+          placeholder={suggestion}
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+          multiline
+          numberOfLines={4}
+          textAlignVertical="top"
+        />
+        {inputValue && (
+          <Text className="w-full my-2">How about💡...{suggestion}</Text>
+        )}
+        <TouchableOpacity
+          className={`w-full h-10 bg-blue-500 rounded text-white text-center p-2 justify-center items-center ${
+            !inputValue && "mt-2"
+          }`}
+          onPress={handleSubmit}
+          disabled={!inputValue}
+        >
+          <Text>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="w-full h-10 bg-green-500 rounded text-white text-center p-2 my-1 justify-center items-center"
+          onPress={handleRefreshSuggestion}
+        >
+          <Text>Gimme a new suggestion</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="w-full h-10 bg-yellow-500 rounded text-white text-center p-2 justify-center items-center"
+          onPress={handleUseSuggestion}
+        >
+          <Text>Use suggestion</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex flex-col items-center justify-center mx-4">
+        <FlatList
+          data={images}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.name}
+        />
+      </View>
+      <TouchableOpacity
+        className="absolute bottom-4 right-4 bg-green-500 rounded text-white text-center p-2 justify-center items-center"
+        onPress={handleRefreshImage}
+      >
+        <Text>Refresh Image</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/fetchData.ts
+++ src/fetchData.ts
@@ -9,6 +9,7 @@ interface ImageUrlsResponse {
 export const fetchImages = async () => {
   try {
     const response = await ApiService.get<ImageUrlsResponse>(API_GET_IMAGES);
+    response.imageUrls && console.log("Fetched images");
     return response.imageUrls;
   } catch (error) {
     console.error(error);
@@ -18,7 +19,8 @@ export const fetchImages = async () => {
 export const fetchSuggestion = async () => {
   try {
     const response = await ApiService.get<string>(API_GET_SUGGESTIONS);
-    return response;
+    response && console.log("Fetched suggestion.");
+    return response.trim();
   } catch (error) {
     console.log(error);
   }
