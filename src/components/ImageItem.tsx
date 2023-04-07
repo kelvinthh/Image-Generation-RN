e2190a0aa70d051e2d1f040d717adbf5847706b8
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2023-04-07 02:04:17 -0400

    Update ImageItem.tsx
    - Add hold to show image name

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, Image, ActivityIndicator } from "react-native";
+import {
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { ImageUrl } from "../types/imageUrl";
 
 interface ImageItemProps {
@@ -8,6 +14,11 @@ interface ImageItemProps {
 
 const ImageItem: React.FC<ImageItemProps> = ({ item }) => {
   const [loading, setLoading] = useState(true);
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const handleLongPress = () => {
+    setShowOverlay(!showOverlay);
+  };
 
   return (
     <View key={item.name}>
@@ -16,17 +27,29 @@ const ImageItem: React.FC<ImageItemProps> = ({ item }) => {
           <ActivityIndicator size="large" color="#000000" />
         </View>
       )}
-      <Image
-        source={{ uri: item.url }}
+      <TouchableOpacity
+        onPressIn={handleLongPress}
+        onPressOut={handleLongPress}
+        activeOpacity={1}
         style={{
           width: "100%",
           height: undefined,
           aspectRatio: 1,
           opacity: loading ? 0 : 1,
         }}
-        resizeMode="cover"
-        onLoad={() => setLoading(false)}
-      />
+      >
+        <Image
+          source={{ uri: item.url }}
+          style={{ flex: 1 }}
+          resizeMode="cover"
+          onLoad={() => setLoading(false)}
+        />
+        {showOverlay && (
+          <View className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-70 justify-center items-center p-4">
+            <Text className='text-center font-light'>{item.name.split('_')[0].trim()}</Text>
+          </View>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
